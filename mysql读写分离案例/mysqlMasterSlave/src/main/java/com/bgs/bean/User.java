901e10951b134145f8fbbc04b2d5fe10cdf298c4
mqf <743731014@qq.com>
2018-03-24 17:42:30 +0800

    【新增】 mysql读写分离项目案例

---
+++ mysql读写分离案例/mysqlMasterSlave/src/main/java/com/bgs/bean/User.java
@@ -0,0 +1,53 @@
+package com.bgs.bean;
+
+public class User {
+	private int id;
+	private String username;
+	private String password;
+	private String sex;
+	private int age;
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public User(int id, String username, String password, String sex, int age) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.sex = sex;
+		this.age = age;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+}
